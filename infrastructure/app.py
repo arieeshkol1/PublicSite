228f64ee6c8a8d2855473748477f96fa89f18f5f
arieeshkol1 <ariel.eshkol@gmail.com>
2025-08-26 14:04:26 +0300

    Update app.py

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python3
-import os
-import aws_cdk as cdk
+from aws_cdk import App, Environment
 from stacks.network_stack import NetworkStack
 from stacks.storage_stack import StorageStack
 from stacks.compute_stack import ComputeStack
 from stacks.iam_ci_stack import IamCiStack
 
-app = cdk.App()
+app = App()
 
-env = cdk.Environment(
-    account=os.getenv("CDK_DEFAULT_ACCOUNT"),
-    region=os.getenv("CDK_DEFAULT_REGION", "us-east-1"),
-)
+env = Environment(account="991105135552", region="us-east-1")  # <— pin here
 
 network = NetworkStack(app, "TSG-Network", env=env)
-storage = StorageStack(app, "TSG-Storage", vpc=network.vpc, env=env)
+storage = StorageStack(app, "TSG-Storage", env=env)
 compute = ComputeStack(app, "TSG-Compute", vpc=network.vpc, buckets=storage.buckets, env=env)
 iam_ci = IamCiStack(app, "TSG-IamCi", env=env)
 
